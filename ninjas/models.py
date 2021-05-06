294887ed8a7c42a079b74bb4565f9bdb986ca539
Funnick <marhdez1999@gmail.com>
2021-05-06 13:08:11 -0400

    invocations

--- ninjas/models.py
+++ ninjas/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from skills.models import Skill
+from invocations.models import Invocation
 
 # Create your models here.
 class Ninja(models.Model):
@@ -8,9 +9,13 @@ class Ninja(models.Model):
     age = models.IntegerField()
     clan = models.CharField(max_length=200)
     birth_date = models.DateTimeField('birth')
+    gender = models.CharField(max_length=10)
 
     #ninja skill relationship
     skills = models.ManyToManyField(Skill)
+
+    #ninja invocation relationship
+    invocations = models.ManyToManyField(Invocation)
 
     def __str__(self):
         return self.name
